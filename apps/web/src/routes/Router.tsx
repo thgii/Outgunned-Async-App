262953d5596759e1aeb9c8cfd4a39fbf71528717
thgii <thomashgrahamii@gmail.com>
2025-10-18 11:36:33 -0600

    removed character edit-handled on character sheet.

--- apps/web/src/routes/Router.tsx
+++ apps/web/src/routes/Router.tsx
@@ -5,7 +5,6 @@ import Game from "./Game";
 import Character from "./Character";
 import CharacterCreator from "./CharacterCreator";
 import CharactersList from "./CharactersList";
-import CharacterEdit from "./CharacterEdit";
 
 import Header from "../components/Header";
 
@@ -22,7 +21,6 @@ export const Router = () => (
           <Route path="/campaign/:id" element={<Campaign />} />
           <Route path="/game/:id" element={<Game />} />
           <Route path="/character/:id" element={<Character />} />
-          <Route path="/character/:id/edit" element={<CharacterEdit />} />
           <Route path="/characters/new" element={<CharacterCreator />} />
           <Route path="/characters" element={<CharactersList />} />
         </Routes>
