41da6a7aa0b8bc96d491749c1f9179fbf87b805f
thgii <thomashgrahamii@gmail.com>
2025-10-20 08:04:23 -0600

    Revert "fix react input crash"
    This reverts commit 056c7231cb10a19025da35f24c39cc6bcfbad24b.

--- apps/web/src/components/CharacterWizard.tsx
+++ apps/web/src/components/CharacterWizard.tsx
@@ -1,7 +1,8 @@
-import React, { useMemo, useState, useEffect } from "react";
+import { useMemo, useState } from "react";
 import type { CharacterDTO, SkillKey, AttrKey } from "@action-thread/types";
 import { api } from "../lib/api";
 import { DATA, findRole, findTrope, buildDerivedDTO, featsAllowanceByAge, featRules, roleOptionLists, isSpecialRole, FEAT_DESC, isNpcSpecialRole } from "../data/wizard";
+import { useEffect } from "react";
 
 const [npcSpecialAttrs, setNpcSpecialAttrs] = useState<AttrKey[]>([]);
 const npcSpecial = isNpcSpecialRole(role);
